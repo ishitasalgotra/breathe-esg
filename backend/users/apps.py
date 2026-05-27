d2743f7a7af35c443952f9e4c7fbf8b0b32fcb94
Ishita Salgotra <ishitasalgotra80@gmail.com>
2026-05-27 23:24:42 +0530

    Auto create users

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -2,6 +2,22 @@
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
 
+    def ready(self):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        if not User.objects.filter(email="admin@example.com").exists():
+            User.objects.create_superuser(
+                email="admin@example.com",
+                password="Admin@12345"
+            )
+
+        if not User.objects.filter(email="analyst@example.com").exists():
+            User.objects.create_user(
+                email="analyst@example.com",
+                password="Password@12345"
+            )
